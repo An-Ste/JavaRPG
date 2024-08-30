fb5aa900f3f203aed536593bb538350265ad9bd6
Kuro <135591555+An-Ste@users.noreply.github.com>
2024-08-30 17:58:59 +0900

    Java RPG 作成中🤣
    クラス周りが難しいヨ！😎

---
+++ Item.java
@@ -0,0 +1,9 @@
+public class Item {
+    private String name;
+
+    public static void potion() {
+        private int p_heal;
+        
+        System.out.println(this.name + "を使用した！");
+    }
+}
