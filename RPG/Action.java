5a031c31c9c43ca56cb3128a1976a8540e46ad73
Kuro <135591555+An-Ste@users.noreply.github.com>
2024-08-30 18:03:19 +0900

    JavaでRPG作成中😎
    クラス関係が難しいヨ🤣

---
+++ RPG/Action.java
@@ -0,0 +1,13 @@
+public class Action {
+    public static void attack() {
+        System.out.println(this.name + "の攻撃！")
+    }
+
+    public static void use_item() {
+        System.out.println(this.name + "は、" + this.name + "を使用した！")
+    }
+
+    public static void stay() {
+
+    }
+}
