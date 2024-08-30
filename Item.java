76e3b99d5a2e460e9ae31d4aa8db7fdd5e9aa64f
Kuro <135591555+An-Ste@users.noreply.github.com>
2024-08-30 18:05:13 +0900

    Delete Item.java

--- Item.java
+++
@@ -1,9 +0,0 @@
-public class Item {
-    private String name;
-
-    public static void potion() {
-        private int p_heal;
-        
-        System.out.println(this.name + "を使用した！");
-    }
-}
