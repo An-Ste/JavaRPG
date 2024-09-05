a4f7e0c296017bef7b5e0ac729bd8d17ecdd0886
Ann <135591555+An-Ste@users.noreply.github.com>
2024-09-05 21:25:06 +0900

    Main

--- RPG/Main.java
+++ RPG/Main.java
@@ -1,41 +1,34 @@
-// 過去のデータ
+package RPG;
 
 public class Main {
+	public static void main (String args[]) {
+		System.out.println("RPG作成中");
+		
+		Ally ally = new Ally();
+		Enemy enemy = new Enemy();
+		
+		// ポーションの処理
+		while (ally.item_num >= 1 || enemy.item_num >= 1) {
+			if (0 < ally.item_num) {
+				ally.use_potion();
+			} else {
+				System.out.println(ally.name + " のポーションは無くなりました！使えませんね！");
+			}
+			
+			if (0 < enemy.item_num) {
+				enemy.use_potion();
+			} else {
+				System.out.println(enemy.name + " のポーションは無くなりました！使えませんね！");
+			}
+		}
+		
+		// 攻撃処理
+//		while (ally.hp < 0 && enemy.hp < 0) {
+//			ally_attack();
+//			enemy_attack();
+//		}
+//		enemy = ally.attack(enemy);
 
-	// public static void main(String[] args) {
 
-	// 	// RPG
-
-	// }
-
-	// class Character {
-	// 	public static Chara_Status (attack, defense, magic_at, magic_df, speed, luck, tech, hp, move) {
-
-	// 	}
-	// 	public static Chara_Items (slot1, slot2, slot3, slot4, slot5, slot6) {
-
-	// 	}
-	// 	public static Chara_Skill (uni_skill, ord_skill1, ord_skill2, ord_skill3) {
-
-	// 	}
-	// }
-
-	// class Arms {
-	// 	public static Arms_Status (name, attack, hit, kill, avoid, kill_avoid, range) {
-
-	// 	}
-	// }
-
-	// class Action {
-
-	// }
-
-	// class Job {
-
-	// }
-
-	// class Items {
-
-	// }
-
+	}
 }
